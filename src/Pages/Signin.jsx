5c90a723139faa006ee556919bde3740ea25bf86
zihad868 <zihadh868@gmail.com>
2024-04-26 18:03:03 +0600

    Signup & Signin Page

--- src/Pages/Signin.jsx
+++ src/Pages/Signin.jsx
@@ -1,10 +1,81 @@
+import { useForm } from "react-hook-form"
+import { Link } from "react-router-dom";
+import gitHub from '../../src/assets/Logo/github.svg';
+import google from '../../src/assets/Logo/google.png';
 
 const Signin = () => {
-    return (
-        <div>
-            <h3>Sign in </h3>
+
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+      } = useForm()
+
+    const onSubmit = (data) => console.log(data)
+  return (
+    <div className="hero min-h-screen bg-base-200">
+      <div className="hero-content flex-col">
+        <div className="text-center lg:text-left">
+          <h1 className="text-5xl font-bold">Sign in now!</h1>
         </div>
-    );
+        <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+          <form onSubmit={handleSubmit(onSubmit)} className="card-body">
+
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Email</span>
+              </label>
+              <input
+                {...register("email", { required: true })}
+                className="border border-gray-300 rounded-md px-4 py-2 focus:outline-none focus:border-gray-500"
+              />
+              {errors.email && (
+                <span className="text-red-500">This field is required</span>
+              )}
+            </div>
+
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Password</span>
+              </label>
+              <input
+                {...register("password", { required: true })}
+                className="border border-gray-300 rounded-md px-4 py-2 focus:outline-none focus:border-gray-500"
+              />
+              {errors.password && (
+                <span className="text-red-500">This field is required</span>
+              )}
+            </div>
+
+            <div className="form-control mt-6">
+              <button className="btn btn-primary">Sign-up</button>
+            </div>
+          </form>
+
+          <div className="flex space-x-2 m-5">
+          <div  className="flex bg-gray-300 p-2 rounded-lg space-x-1">
+            <img className="w-10 h-10" src={google} alt="" />
+            <button >Google Signup</button>
+          </div>
+
+          <div className="flex bg-gray-300 p-2 rounded-lg space-x-2">
+            <img className="w-10 h-10" src={gitHub} alt="" />
+            <button >GitHub Signup</button>
+          </div>
+          </div>
+
+          <div>
+            <p className="text-center mb-6">
+              You don't have an account?{" "}
+              <Link className="text-primary font-bold" to="/signup">
+                Signin
+              </Link>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Signin;
--- src/Pages/Signup.jsx
+++ src/Pages/Signup.jsx
@@ -1,45 +1,78 @@
+import { useForm } from "react-hook-form"
+import { Link } from "react-router-dom";
+
 const Signup = () => {
+
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+      } = useForm()
+
+      const onSubmit = (data) => console.log(data)
+
   return (
     <div className="hero min-h-screen bg-base-200">
-      <div className="hero-content flex-col lg:flex-row-reverse">
+      <div className="hero-content flex-col">
         <div className="text-center lg:text-left">
-          <h1 className="text-5xl font-bold">Login now!</h1>
-          <p className="py-6">
-            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
-            a id nisi.
-          </p>
+          <h1 className="text-5xl font-bold">Sign up now!</h1>
+
         </div>
         <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-          <form className="card-body">
+          <form onSubmit={handleSubmit(onSubmit)} className="card-body">
+
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Name</span>
+              </label>
+              <input {...register("name", { required: true })} 
+                className="border border-gray-300 rounded-md px-4 py-2 focus:outline-none focus:border-gray-500"
+              />
+              {errors.name && <span className="text-red-500">This field is required</span>}
+              
+            </div> 
+          
             <div className="form-control">
               <label className="label">
                 <span className="label-text">Email</span>
               </label>
-              <input {...register("exampleRequired", { required: true })} />
-              {/* errors will return when field validation fails  */}
-              {errors.exampleRequired && <span>This field is required</span>}
+              <input {...register("email", { required: true })} 
+                className="border border-gray-300 rounded-md px-4 py-2 focus:outline-none focus:border-gray-500"
+              />
+              {errors.email && <span className="text-red-500">This field is required</span>}
+              
             </div>
+
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Photo</span>
+              </label>
+              <input {...register("photo", { required: true })} 
+                className="border border-gray-300 rounded-md px-4 py-2 focus:outline-none focus:border-gray-500"
+              />
+              {errors.photo && <span className="text-red-500">This field is required</span>}
+              
+            </div>
+
             <div className="form-control">
               <label className="label">
                 <span className="label-text">Password</span>
               </label>
-              <input
-                type="password"
-                placeholder="password"
-                className="input input-bordered"
-                required
+              <input {...register("password", { required: true })} 
+                className="border border-gray-300 rounded-md px-4 py-2 focus:outline-none focus:border-gray-500"
               />
-              <label className="label">
-                <a href="#" className="label-text-alt link link-hover">
-                  Forgot password?
-                </a>
-              </label>
+              {errors.password && <span className="text-red-500">This field is required</span>}
+              
             </div>
+
             <div className="form-control mt-6">
-              <button className="btn btn-primary">Login</button>
+              <button className="btn btn-primary">Sign-up</button>
             </div>
           </form>
+
+          <div>
+            <p className="text-center mb-6">Already have an account? <Link className="text-primary font-bold" to='/signin'>Signin</Link></p>
+          </div>
         </div>
       </div>
     </div>
