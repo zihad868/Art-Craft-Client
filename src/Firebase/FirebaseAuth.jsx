47c985ba039497a651cc44ccf8858222a6a7620e
zihad868 <zihadh868@gmail.com>
2024-04-26 18:11:16 +0600

    Firebase Configuration

---
+++ src/Firebase/FirebaseAuth.jsx
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCS_ABrLgcOg52WYscO63cI6lF0nQJLyuQ",
+  authDomain: "artcraft-9fe4c.firebaseapp.com",
+  projectId: "artcraft-9fe4c",
+  storageBucket: "artcraft-9fe4c.appspot.com",
+  messagingSenderId: "991046876002",
+  appId: "1:991046876002:web:158ce85ea4f424fc038a3b"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
