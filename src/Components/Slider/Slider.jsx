f75c30c4b409699397c558e092c17ef8399d5d7c
zihad868 <zihadh868@gmail.com>
2024-04-28 09:52:47 +0600

    Update Slider

--- src/Components/Slider/Slider.jsx
+++ src/Components/Slider/Slider.jsx
@@ -9,11 +9,12 @@ import "swiper/css";
 
 const Slider = () => {
   return (
-    <div>
+    <div className="mt-8">
       <div>
-        
+        <h3 className="text-4xl font-bold text-center">Jute & wooden Crafts </h3>
+        <p className="text-center w-10/12 mx-auto mb-5">Explore our exquisite collection of handcrafted jute and wooden crafts, meticulously crafted with sustainable materials, offering eco-friendly decor and accessories that exude rustic charm and timeless elegance for your home.</p>
       </div>
-      <div>
+      <div className="m-2">
         <Swiper
           spaceBetween={50}
           slidesPerView={2}
