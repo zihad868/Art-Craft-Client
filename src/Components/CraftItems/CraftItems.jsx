722e2d77b764aa0dc65c4a7d5162ec027525afa9
zihad868 <zihadh868@gmail.com>
2024-04-30 20:06:22 +0600

    Update Code

--- src/Components/CraftItems/CraftItems.jsx
+++ src/Components/CraftItems/CraftItems.jsx
@@ -14,7 +14,7 @@ const CraftItems = () => {
     return (
         <div className="mb-8 flex flex-col items-center">
             <h3 className="text-4xl font-bold text-center text-sky-400"> <Zoom>Craft Items</Zoom></h3>
-            <div className="grid sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-2 gap-5 border-red-500 mt-5">
+            <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 border-red-500 m-4">
                 {
                     crafts.map(craft => <CraftCard key={craft._id} craft={craft}/>)
                 }
