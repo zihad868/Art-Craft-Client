2a5cce4f8aa16b163f82cbf1d64a85a61551fe35
zihad868 <zihadh868@gmail.com>
2024-04-30 15:45:12 +0600

    Update

--- src/Components/Contact/ContactCard.jsx
+++ src/Components/Contact/ContactCard.jsx
@@ -1,14 +1,16 @@
+import { Link } from "react-router-dom";
 
 const ContactCard = ({ craft }) => {
     console.log(craft)
-    const {photo, subCategoryName, email, shortDescription } = craft;
+    const {_id ,photo, subCategoryName, email, shortDescription } = craft;
     return (
         <div className="">
             <img className="w-[400px] h-[300px]" src={photo} alt="" />
             <div className="">
                 <p> <span className="text-xl">Category:</span> {subCategoryName}</p>
                 <p> <span className="text-xl">Email:</span> {email}</p>
-                <p> <span className="text-xl">Description:</span> {shortDescription.slice(0, 50)}</p>
+                <p> <span className="text-xl">Description:</span> {shortDescription.slice(0, 50)}</p>     
+                <Link to={`/craft-details/${_id}`}><button className="bg-green-400 p-2 w-full rounded-lg">View Details</button></Link>
             </div>
         </div>
     );
