a90b59316ac21c7805c51333a6d3e1038723c415
zihad868 <zihadh868@gmail.com>
2024-04-30 19:56:18 +0600

    Light and Dark Theme

--- src/Components/Navbar/Nav.jsx
+++ src/Components/Navbar/Nav.jsx
@@ -1,11 +1,28 @@
 import { Link, NavLink } from "react-router-dom";
 import logo from "../../assets/Logo/ArtCraft.jpeg";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { AuthProvider } from "../../Provider/FirebaseAuthProvider";
 import { Typewriter } from "react-simple-typewriter";
 
 const Nav = () => {
   const { user, logoutUser } = useContext(AuthProvider);
+  const [theme, setTheme] = useState('light');
+
+  const handleTheme = (e) => {
+    if(e.target.checked){
+        setTheme('synthwave')
+    }
+    else{
+        setTheme('ligth')
+    }
+  }
+
+  useEffect(() =>{
+    localStorage.setItem('theme', theme);
+    const themeColor = localStorage.getItem('theme');
+    document.querySelector('html').setAttribute('data-theme', themeColor);
+  },[theme]
+  )
 
   const handleLogout = () => {
     logoutUser()
@@ -147,6 +164,44 @@ const Nav = () => {
                 </div>
               </>
             )}
+
+            {/* Theme Controller */}
+            <label className="cursor-pointer grid place-items-center p-2">
+              <input
+                type="checkbox"
+                onChange={handleTheme}
+                className="toggle theme-controller bg-base-content row-start-1 col-start-1 col-span-2"
+              />
+              <svg
+                className="col-start-1 row-start-1 stroke-base-100 fill-base-100"
+                xmlns="http://www.w3.org/2000/svg"
+                width="14"
+                height="14"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              >
+                <circle cx="12" cy="12" r="5" />
+                <path d="M12 1v2M12 21v2M4.2 4.2l1.4 1.4M18.4 18.4l1.4 1.4M1 12h2M21 12h2M4.2 19.8l1.4-1.4M18.4 5.6l1.4-1.4" />
+              </svg>
+              <svg
+                className="col-start-2 row-start-1 stroke-base-100 fill-base-100"
+                xmlns="http://www.w3.org/2000/svg"
+                width="14"
+                height="14"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              >
+                <path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path>
+              </svg>
+            </label>
           </div>
     </div>
   );
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,12 +1,30 @@
 import "./Navbar.css";
 import logo from "../../assets/Logo/ArtCraft.jpeg";
 import { Link, NavLink } from "react-router-dom";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { AuthProvider } from "../../Provider/FirebaseAuthProvider";
 import { Typewriter } from "react-simple-typewriter";
 
 const Navbar = () => {
   const { user, logoutUser } = useContext(AuthProvider);
+  const [theme, setTheme] = useState('light');
+
+  const handleTheme = (e) => {
+    if(e.target.checked){
+        setTheme('synthwave')
+    }
+    else{
+        setTheme('ligth')
+    }
+  }
+
+  useEffect(() =>{
+    localStorage.setItem('theme', theme);
+    const themeColor = localStorage.getItem('theme');
+    document.querySelector('html').setAttribute('data-theme', themeColor);
+  },[theme]
+  )
+
 
   const handleLogout = () => {
     logoutUser();
@@ -46,7 +64,7 @@ const Navbar = () => {
           isActive ? "bg-sky-300 rounded-lg p-2" : "p-2"
         }
       >
-        Craft Design 
+        Craft Design
       </NavLink>
 
       <NavLink
@@ -143,6 +161,44 @@ const Navbar = () => {
                 </div>
               </>
             )}
+
+            {/* Theme Controller */}
+            <label className="cursor-pointer grid place-items-center p-2">
+              <input
+                type="checkbox"
+                onChange={handleTheme}
+                className="toggle theme-controller bg-base-content row-start-1 col-start-1 col-span-2"
+              />
+              <svg
+                className="col-start-1 row-start-1 stroke-base-100 fill-base-100"
+                xmlns="http://www.w3.org/2000/svg"
+                width="14"
+                height="14"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              >
+                <circle cx="12" cy="12" r="5" />
+                <path d="M12 1v2M12 21v2M4.2 4.2l1.4 1.4M18.4 18.4l1.4 1.4M1 12h2M21 12h2M4.2 19.8l1.4-1.4M18.4 5.6l1.4-1.4" />
+              </svg>
+              <svg
+                className="col-start-2 row-start-1 stroke-base-100 fill-base-100"
+                xmlns="http://www.w3.org/2000/svg"
+                width="14"
+                height="14"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              >
+                <path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path>
+              </svg>
+            </label>
           </div>
         </div>
       </div>
